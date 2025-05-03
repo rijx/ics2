f225db68aae8e903b90ff693670f618599401b2e
Mitchell de Rijcke <mdr@rijx.com>
2025-05-03 04:48:21 +0200

    Add SCHEDULE-AGENT support

--- src/utils/set-contact.js
+++ src/utils/set-contact.js
@@ -1,8 +1,9 @@
-export default function setContact({ name, email, rsvp, dir, partstat, role }) {
+export default function setContact({ name, email, rsvp, dir, partstat, role, scheduleAgent }) {
   let formattedAttendee = ''
   formattedAttendee += rsvp ? 'RSVP=TRUE;' : 'RSVP=FALSE;'
   formattedAttendee += role ? `ROLE=${role};` : ''
   formattedAttendee += partstat ? `PARTSTAT=${partstat};` : ''
+  formattedAttendee += scheduleAgent ? `SCHEDULE-AGENT=${scheduleAgent};` : ''
   formattedAttendee += dir ? `DIR=${dir};` : ''
   formattedAttendee += 'CN='
   formattedAttendee += name || 'Unnamed attendee'
